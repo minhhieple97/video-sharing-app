673ec276122cc70064521fd478adccecc7a2df60
hieple <hieplevuc@gmail.com>
2024-08-29 21:20:37 +0700

    feat: Setup prisma integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BullQueueModule } from './bullqueue/bullqueue.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BullQueueModule],
+  imports: [PrismaModule, BullQueueModule],
   controllers: [AppController],
   providers: [AppService],
 })
