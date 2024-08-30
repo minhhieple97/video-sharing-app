93addd823e4a30cfadf25403b3783be1f6a1b85f
hieple <hieplevuc@gmail.com>
2024-08-30 11:33:05 +0700

    feat: Add middleware for prefix api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
