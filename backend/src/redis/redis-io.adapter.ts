9d029adb476428b3d4a6a939653007246f08edc9
hieple <hieplevuc@gmail.com>
2024-08-30 18:04:35 +0700

    feat: Add redis adapter

---
+++ backend/src/redis/redis-io.adapter.ts
@@ -0,0 +1,39 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+import { INestApplication, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+export class RedisIoAdapter extends IoAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter>;
+  private configService: ConfigService;
+  constructor(app: INestApplication) {
+    super(app);
+    this.configService = app.get(ConfigService);
+  }
+
+  async connectToRedis(): Promise<void> {
+    try {
+      const pubClient = createClient({
+        socket: {
+          host: this.configService.get('REDIS_HOST'),
+          port: this.configService.get('REDIS_PORT'),
+        },
+      });
+      const subClient = pubClient.duplicate();
+
+      await Promise.all([pubClient.connect(), subClient.connect()]);
+      this.adapterConstructor = createAdapter(pubClient, subClient);
+      Logger.log('Redis connection successful');
+    } catch (error) {
+      console.error('Error connecting to Redis', error);
+    }
+  }
+
+  createIOServer(port: number, options?: ServerOptions): any {
+    const server = super.createIOServer(port, options);
+    server.adapter(this.adapterConstructor);
+    return server;
+  }
+}
