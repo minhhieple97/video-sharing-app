0c9c0b66e1d577490c94bbba5cf54115becbe37f
hieple <hieplevuc@gmail.com>
2024-09-03 07:55:03 +0700

    feat: Setup extract jwt cookie  in ws middlleware

--- src/notification/middlewares/auth-ws.middleware.ts
+++ src/notification/middlewares/auth-ws.middleware.ts
@@ -1,17 +1,18 @@
 import { JwtService } from '@nestjs/jwt';
+import { IncomingHttpHeaders } from 'http';
 import { Socket } from 'socket.io';
 import { JwtTokenPayload } from 'src/auth/interfaces';
+import { JWT_COOKIE_NAME } from 'src/constants';
 
 type SocketMiddleware = (socket: Socket, next: (err?: Error) => void) => void;
 
 export const AuthWsMiddleware = (jwtService: JwtService): SocketMiddleware => {
   return async (socket: Socket, next) => {
     try {
-      const token = socket.handshake?.headers?.authorization;
+      const token = extractAccessToken(socket.handshake.headers);
       if (!token) {
         throw new Error('Authorization token is missing');
       }
-
       let payload: JwtTokenPayload | null = null;
 
       try {
@@ -32,4 +33,19 @@ export const AuthWsMiddleware = (jwtService: JwtService): SocketMiddleware => {
       next(new Error('Unauthorized'));
     }
   };
+};
+
+export const extractAccessToken = (
+  header: IncomingHttpHeaders,
+): string | null => {
+  const cookieString = header.cookie;
+  if (!cookieString) {
+    return null;
+  }
+  const cookies = cookieString.split(';').reduce((acc, cookie) => {
+    const [key, value] = cookie.trim().split('=');
+    acc[key] = value;
+    return acc;
+  }, {});
+  return cookies[JWT_COOKIE_NAME] || null;
 };
