1c7b78cdf3add0f9200924ea23316a927df47285
hieple <hieplevuc@gmail.com>
2024-09-04 09:18:50 +0700

    refactor: Remove bullmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { BullQueueModule } from './bullqueue/bullqueue.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
@@ -11,7 +10,6 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule,
     PrismaModule,
-    BullQueueModule,
     AuthModule,
     VideoModule,
     NotificationModule,
