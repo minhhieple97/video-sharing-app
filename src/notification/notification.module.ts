b2c76e345731676f4112cea79ecd7907d5b4c43f
hieple <hieplevuc@gmail.com>
2024-09-04 08:23:46 +0700

    feat: Add redis moudle to notification module

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,10 @@
+import { RedisModule } from './../redis/redis.module';
 import { Module } from '@nestjs/common';
 import { NotificationGateway } from './notification.gateway';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, RedisModule],
   controllers: [],
   providers: [NotificationGateway],
   exports: [NotificationGateway],
